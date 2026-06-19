30760bdc309bffa22aad7544ccaff277e75a7c7d
Sergei Repnikov <rpnkv@users.noreply.github.com>
2026-06-19 12:52:36 +0300

    Add: online-stock-span - submission-3

---
+++ Data Structures & Algorithms/online-stock-span/submission-3.py
@@ -0,0 +1,16 @@
+class StockSpanner:
+    def __init__(self):
+        self.stack = []
+
+    def next(self, price: int) -> int:
+        stack = self.stack
+        
+        days = 1
+
+        while stack and stack[-1][0] <= price:
+            days += stack.pop()[1]
+        
+        stack.append((price, days))
+
+        return days
+        
