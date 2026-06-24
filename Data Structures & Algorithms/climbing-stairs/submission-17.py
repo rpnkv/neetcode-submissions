23892b01d215389e610636a944f8593e1eaa550e
Sergei Repnikov <rpnkv@users.noreply.github.com>
2026-06-24 09:18:34 +0300

    Add: climbing-stairs - submission-17

---
+++ Data Structures & Algorithms/climbing-stairs/submission-17.py
@@ -0,0 +1,12 @@
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        dp = [0, 1, 2] + [None] * (n - 2)
+
+        def dfs(n: int) -> int:
+            if not dp[n]:
+                dp[n] = dfs(n - 1) + dfs(n - 2)
+
+            return dp[n]
+
+        return dfs(n)
