b4e1c390a141d5d4b5e4ccc3f5925320905a587c
Sergei Repnikov <rpnkv@users.noreply.github.com>
2026-06-24 09:13:23 +0300

    Add: climbing-stairs - submission-14

---
+++ Data Structures & Algorithms/climbing-stairs/submission-14.py
@@ -0,0 +1,8 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n < 3:
+            return n
+        else:
+            return self.climbStairs(n - 1) + self.climbStairs(n - 2)
+
+        
