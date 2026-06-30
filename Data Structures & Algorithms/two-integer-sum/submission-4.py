ccc6ccbbda09b5dc5bfc4612a26cc39d003f542a
Sergei Repnikov <rpnkv@users.noreply.github.com>
2026-06-30 12:26:50 +0300

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.py
@@ -0,0 +1,17 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        s = set(nums)
+
+        for i, n in enumerate(nums):
+            if target - n in s:
+                if n * 2 == target:
+                    nums.remove(n)
+                    return [n, nums.index(n) + 1]
+                else:
+                    return [i, nums.index(target - n)]
+
+        
+        raise Exception('unexpected state')
+
+
+        
