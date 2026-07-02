8ca062765774cf8b7acc0b9302803ac913144e7f
Sergei Repnikov <rpnkv@users.noreply.github.com>
2026-07-02 13:47:44 +0300

    Add: insert-new-interval - submission-2

---
+++ Data Structures & Algorithms/insert-new-interval/submission-2.py
@@ -0,0 +1,19 @@
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        res = []
+
+        new_start, new_end = newInterval
+
+        for start, end in intervals:
+            if new_start <= end and start <= new_end:
+                new_start = min(start, new_start)
+                new_end = max(end, new_end)
+                continue
+            else:
+                res.append([start, end])
+
+        res.append([new_start, new_end])
+
+        res.sort(key=lambda i: i[0])
+
+        return res
