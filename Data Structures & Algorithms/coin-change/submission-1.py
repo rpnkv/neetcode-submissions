7916f795328dc0d01e729a6b4860273a6465bb25
Sergei Repnikov <rpnkv@users.noreply.github.com>
2026-07-06 13:43:55 +0300

    Add: coin-change - submission-1

---
+++ Data Structures & Algorithms/coin-change/submission-1.py
@@ -0,0 +1,31 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        if amount == 0:
+            return 0
+
+        dp = [-1] * (amount + 1)
+        dp[0] = 0
+
+        for coin in coins:
+            dp[coin] = 1
+
+        coins.sort(reverse=True)
+
+        for i in range(1, amount + 1):
+            if i in dp:
+                continue
+
+            for coin in coins:
+                candidate = i - coin
+                if candidate < 0:
+                    continue
+
+                if dp[candidate] != -1:
+                    dp[i] = dp[candidate] + 1
+                    break
+
+        return dp[-1]
+
+
+
+
