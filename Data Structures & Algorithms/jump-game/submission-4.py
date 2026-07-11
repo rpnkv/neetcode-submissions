ba82096919c0068cba8eea7e1e540bdce2793eca
Sergei Repnikov <rpnkv@users.noreply.github.com>
2026-07-11 12:48:14 +0300

    Add: jump-game - submission-4

---
+++ Data Structures & Algorithms/jump-game/submission-4.py
@@ -0,0 +1,11 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        threshold = 0
+
+        for i, n in enumerate(nums):
+            if i <= threshold:
+                threshold = max(threshold, i + n)
+            else:
+                return False
+
+        return True
