e2b9acc2ba9036de48080cc7d41a63c7829d43e3
Sergei Repnikov <rpnkv@users.noreply.github.com>
2026-07-12 13:19:47 +0300

    Add: is-anagram - submission-8

---
+++ Data Structures & Algorithms/is-anagram/submission-8.py
@@ -0,0 +1,15 @@
+from collections import Counter
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_c = Counter(s)
+
+        for char in t:
+            if not char in s_c:
+                return False
+            else:
+                if s_c[char] == 1:
+                    del s_c[char]
+                else:
+                    s_c[char] -= 1
+        
+        return len(s_c) == 0
