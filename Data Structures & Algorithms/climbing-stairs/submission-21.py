1d82b209fb2433011d832d03d7732245bebb94cf
Sergei Repnikov <rpnkv@users.noreply.github.com>
2026-07-31 18:08:51 +0300

    Add: climbing-stairs - submission-21

---
+++ Data Structures & Algorithms/climbing-stairs/submission-21.py
@@ -0,0 +1,10 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n < 3:
+            return n
+        
+        a, b = 1, 2
+        for i in range(2, n):
+            a, b = b, a + b
+
+        return b
