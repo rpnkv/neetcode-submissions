bb2e4f0bb69c1993a29a96f2a7c6d3b4f1e96162
Sergei Repnikov <rpnkv@users.noreply.github.com>
2026-08-03 13:46:21 +0300

    Add: minimum-window-with-characters - submission-1

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-1.py
@@ -0,0 +1,37 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        # concepts
+        # 1. iterate to first char, present in t using while loop
+        # 2. we can now expand our window
+        # 1.
+        l = r = 0
+        allowed_set = set(t)
+        window_set = set()
+
+        res = ""
+
+        while l < len(s) and s[l] not in t:
+            l += 1
+
+        # 2.
+        for r in range(l, len(s)):
+            char = s[r]
+            if char in allowed_set:
+                if char not in window_set:
+                    window_set.add(char)
+                else:
+                    if s[l] == char:
+                        l += 1
+                        while s[l] not in allowed_set:
+                            l += 1
+
+            if len(window_set) == len(allowed_set):
+                if res == "":
+                    res = s[l: r + 1]
+                    continue
+
+                elif len(res) > (r - l + 1):
+                    res = s[l: r + 1]
+                    continue
+
+        return res
