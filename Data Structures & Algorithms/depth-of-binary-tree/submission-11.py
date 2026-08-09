189279dfdfdc0cf9c20004328e87548ca4af6781
Sergei Repnikov <rpnkv@users.noreply.github.com>
2026-08-09 12:40:31 +0300

    Add: depth-of-binary-tree - submission-11

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-11.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        from collections import deque
+        d = deque()
+
+        if not root:
+            return 0
+        else:
+            d.append(root)
+
+        depth = 0
+        while d:
+            depth += 1
+            for _ in range(len(d)):
+                if d[-1].left:
+                    d.appendleft(d[-1].left)
+                
+                if d[-1].right:
+                    d.appendleft(d[-1].right)
+                d.pop()
+        
+        return depth
