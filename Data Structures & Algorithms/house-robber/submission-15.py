373a3446f2cac109d98f7ac23f8aceb918c7f213
Sergei Repnikov <rpnkv@users.noreply.github.com>
2026-08-22 20:49:15 +0300

    Add: house-robber - submission-15

---
+++ Data Structures & Algorithms/house-robber/submission-15.py
@@ -0,0 +1,17 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        dp = [None] * (len(nums))
+        dp[0], dp[1], dp[2] = nums[0], nums[1], nums[2] + nums[0]
+
+        def dfs(i: int) -> int:
+            if not dp[i]:
+                dp[i] = max(
+                    dp[i - 2],
+                    dp[i - 3]
+                ) + nums[i]
+            
+            return dp[i]
+        
+        dfs(len(nums) - 1)
+        dfs(len(nums) - 2)
+        return max(dp[-1], dp[-2])
